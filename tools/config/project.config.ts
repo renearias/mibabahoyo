c3a41256ad412d9c8a5829c0a3f7846448668ea5
renearias <rene_811@hotmail.com>
2017-01-19 23:03:30 -0500

    v1 mb

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -14,6 +14,8 @@ export class ProjectConfig extends SeedConfig {
   constructor() {
     super();
     // this.APP_TITLE = 'Put name of your app here';
+    
+    this.ENABLE_SCSS = 1;
 
     /* Enable typeless compiler runs (faster) between typed compiler runs. */
     // this.TYPED_COMPILE_INTERVAL = 5;
@@ -21,6 +23,14 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
+      /* Materialize css*/
+     // {src: 'materialize-css/dist/css/materialize.min.css', inject: true},
+      /* Select a pre-built Material theme */
+      //{src: '@angular/material/core/theming/prebuilt/materialize.min.css', inject: true},
+
+      /* HammerJS is required if the app uses certain Material components (eg: md-slider and md-slide-toggle) */
+      //{src: 'hammerjs/hammer.min.js', inject: 'libs'},
+
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
@@ -31,6 +41,29 @@ export class ProjectConfig extends SeedConfig {
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
+    
+    
+    
+    // add flex-layout configuration to SystemJS.
+    this.addPackageBundles({
+      name:'@angular/flex-layout',
+      path:'node_modules/@angular/flex-layout/bundles/flex-layout.umd.js',
+      packageMeta:{
+        main: 'index.js',
+        defaultExtension: 'js'
+      }
+    });
+    
+    // add Material configuration to SystemJS.
+    this.addPackageBundles({
+      name:'@angular/material',
+      path:'node_modules/@angular/material/bundles/material.umd.js',
+      packageMeta:{
+        main: 'index.js',
+        defaultExtension: 'js'
+      }
+    });
+    
    // Add Firebase configuration to SystemJS
     this.addPackageBundles({
       name: 'firebase',
